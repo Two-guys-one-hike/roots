bda62c31bed7bb9eef17e96c56689d0d16544320
pogginicolo98 <pogginicolo@gmail.com>
2024-04-26 23:37:51 +0200

    Fixed refresh token validation

--- apps/account/api/views.py
+++ apps/account/api/views.py
@@ -6,17 +6,17 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from rest_framework_simplejwt.serializers import TokenBlacklistSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+
 class LogoutView(APIView):
     permission_classes = (IsAuthenticated,)
     
     def post(self, request):
         try:
-            serializer = TokenBlacklistSerializer(request.data)
-            serializer.data
-            content = {'message': 'Successfully logged out'}
-            return Response(content)
+            serializer = TokenBlacklistSerializer(data=request.data)
+            if serializer.is_valid():
+                content = {'message': 'Successfully logged out'}
+                return Response(content, status=status.HTTP_205_RESET_CONTENT)            
         except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+            print(e)  # TODO log exception
+        return Response(status=status.HTTP_400_BAD_REQUEST)
